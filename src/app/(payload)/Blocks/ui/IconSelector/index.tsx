527b549856615523a1e56aaa6350ffa461610bce
sourav-vashisht <sourav.vashisht222@gmail.com>
2024-06-05 16:32:11 +0530

    a

--- src/app/(payload)/Blocks/ui/IconSelector/index.tsx
+++ src/app/(payload)/Blocks/ui/IconSelector/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useField } from '@payloadcms/ui/forms';
+// import { useField } from '@payloadcms/ui/forms/useField';
+import { SelectInput } from '@payloadcms/ui/fields/Select';
 import * as FontAwesomeIcons from 'react-icons/fa6';
-import { IconType } from 'react-icons/lib';
+// import { IconType } from 'react-icons/lib';
 
 type Props = {
   path: string;
@@ -10,41 +11,43 @@ type Props = {
 };
 
 export const IconSelector: React.FC<Props> = ({ path, required }) => {
-  const { value, setValue } = useField<string>({ path });
+  // const { value, setValue } = useField<string>({ path });
 
-  const onChange = (value: any) => {
-    console.log(value);
-    setValue(value.value);
-  };
+  // const onChange = (value: any) => {
+  //   setValue(value.value);
+  // };
 
   // const IconComponent = (FontAwesomeIcons as { [key: string]: IconType })?.[value];
 
   return (
-    <div>aa</div>
-    // <SelectInput
-    //   required={required}
-    //   path={path}
-    //   name={path}
-    //   label="Icon"
-    //   value={value}
-    //   options={Object.keys(FontAwesomeIcons).map((value) => ({
-    //     label: value,
-    //     value,
-    //   }))}
-    //   onChange={onChange}
-    //   description={() => (
-    //     <div
-    //       style={{
-    //         display: 'flex',
-    //         justifyContent: 'center',
-    //         alignItems: 'center',
-    //         gap: '5px',
-    //       }}
-    //     >
-    //       Preview:
-    //       {IconComponent ? <IconComponent style={{ width: 25, height: 25 }} /> : null}
-    //     </div>
-    //   )}
-    // />
+    <>
+      <SelectInput
+        required={required}
+        path={path}
+        name={path}
+        label="Icon"
+        value={'aa'}
+        options={Object.keys(FontAwesomeIcons).map((value) => ({
+          label: value,
+          value,
+        }))}
+        onChange={() => {
+          console.log('aa');
+        }}
+        // description={() => (
+        //   <div
+        //     style={{
+        //       display: 'flex',
+        //       justifyContent: 'center',
+        //       alignItems: 'center',
+        //       gap: '5px',
+        //     }}
+        //   >
+        //     Preview:
+        //     {IconComponent ? <IconComponent style={{ width: 25, height: 25 }} /> : null}
+        //   </div>
+        // )}
+      />
+    </>
   );
 };
