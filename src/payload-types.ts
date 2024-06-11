9df4551bcf85ec35148f11a1153cbded64d76a4e
athulpm <97athulpm@gmail.com>
2024-06-11 14:04:18 +0530

    add intro content

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -623,6 +623,22 @@ export interface LogoCloudBlock {
 export interface FormBlock {
   form: string | Form;
   enableIntro?: boolean | null;
+  introContent: {
+    root: {
+      type: string;
+      children: {
+        type: string;
+        version: number;
+        [k: string]: unknown;
+      }[];
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
+      indent: number;
+      version: number;
+    };
+    [k: string]: unknown;
+  };
+  intro_content_html?: string | null;
   id?: string | null;
   blockName?: string | null;
   blockType: 'form-block';
