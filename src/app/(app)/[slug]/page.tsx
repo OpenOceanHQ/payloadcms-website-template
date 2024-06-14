1b1807b11c6350a23c189fd966b5ec34ff1db4df
guri <khalsagudeep@gmail.com>
2024-06-14 12:15:41 +0530

    add metdata and schema

--- src/app/(app)/[slug]/page.tsx
+++ src/app/(app)/[slug]/page.tsx
@@ -1,11 +1,49 @@
-import Header from '../../../common/Components/Header';
-import Footer from '../../../common/Components/Footer';
 import { RenderBlocks } from '../../../common/Blocks/RenderBlocks';
 import configPromise from '@payload-config';
 import { notFound } from 'next/navigation';
 import { getPayload } from 'payload';
 import { ParsedUrlQuery } from 'querystring';
+import { Metadata } from 'next';
+import { siteName } from '../../../common/Components/Header';
+import JsonLdSchema from '@/common/Components/JsonLd';
 
+const serverUrl = process.env.SERVER_URL;
+const getData = async (slug: string) => {
+  const payload = await getPayload({ config: configPromise });
+
+  const { docs } = await payload.find({
+    collection: 'pages',
+    where: { id: { equals: slug } },
+  });
+
+  return docs;
+};
+
+export async function generateMetadata({ params }: { params: ParsedUrlQuery }): Promise<Metadata> {
+  let metadata: Metadata = {};
+  const { slug } = params;
+
+  try {
+    const docs = await getData(typeof slug === 'string' ? slug : slug ? slug[0] : '');
+
+    if (!docs) {
+      throw new Error();
+    }
+
+    metadata = {
+      title: docs[0].title,
+      openGraph: {
+        type: 'website',
+        url: serverUrl,
+        siteName: siteName.siteName,
+      },
+    };
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+  return metadata;
+}
 const page = async ({ params }: { params: ParsedUrlQuery }) => {
   const { slug } = params;
   const payload = await getPayload({ config: configPromise });
@@ -19,10 +57,16 @@ const page = async ({ params }: { params: ParsedUrlQuery }) => {
     if (docs.length === 0) {
       throw new Error();
     }
+    const jsonLdData = {
+      url: serverUrl + docs[0].slug,
+      name: docs[0].title ? docs[0].title : '',
+      datePublished: docs[0].createdAt,
+      dateModified: docs[0].updatedAt,
+    };
 
     return (
       <>
-        <Header /> <RenderBlocks data={docs[0].layout} /> <Footer />
+        <JsonLdSchema {...jsonLdData} /> <RenderBlocks data={docs[0].layout} />
       </>
     );
   } catch (error) {
