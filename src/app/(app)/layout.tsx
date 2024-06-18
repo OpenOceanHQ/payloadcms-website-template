54a3d1e028e0b5d50d63add30c36c702df2f7eba
sourav-vashisht <sourav.vashisht222@gmail.com>
2024-06-18 13:18:07 +0530

    Adding Top Progress Bar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,9 +1,13 @@
 import './global.css';
+import NextTopLoader from 'nextjs-toploader';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head></head>
-      <body>{children}</body>
+      <body>
+        <NextTopLoader color="#f11946" showSpinner={false} />
+        {children}
+      </body>
     </html>
   );
 }
