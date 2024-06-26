06a33c7b2fd372bde69c8e8b77e7f1eeb12ac75d
sourav-vashisht <sourav.vashisht222@gmail.com>
2024-06-26 12:06:43 +0530

    Adding GCP

--- src/utilities/beforeChangeHook.ts
+++ src/utilities/beforeChangeHook.ts
@@ -1,30 +1,53 @@
 import { CollectionBeforeChangeHook } from 'payload/types';
 import { S3 } from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
+import { Storage } from '@google-cloud/storage';
 
 export const beforeChangeHook: CollectionBeforeChangeHook = async ({ data, req }) => {
   try {
-    const s3 = new S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
+    if (process.env.AWS_BUCKET_NAME) {
+      const s3 = new S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION,
+      });
+
+      const fileName = req.file?.name;
+      const randomString = uuidv4();
+      const fileExtension = fileName?.split('.').pop();
+      const newFileName = fileExtension ? `${randomString}.${fileExtension}` : randomString;
+
+      const uploadParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: newFileName,
+        Body: req.file?.data,
+        ContentType: req.file?.mimetype,
+      };
+
+      const d = await s3.upload(uploadParams).promise();
+
+      return { ...data, url: d.Location };
+    }
+
+    // Initialize storage
+    const storage = new Storage({
+      // keyFilename: `./service-account.json`,
+      credentials: JSON.parse(process.env.GCS_CREDENTIALS || '{}'),
     });
 
-    const fileName = req.file?.name;
-    const randomString = uuidv4();
-    const fileExtension = fileName?.split('.').pop();
-    const newFileName = fileExtension ? `${randomString}.${fileExtension}` : randomString;
+    //
 
-    const uploadParams = {
-      Bucket: process.env.AWS_BUCKET_NAME!,
-      Key: newFileName,
-      Body: req.file?.data,
-      ContentType: req.file?.mimetype,
-    };
+    const bucketName = process.env.GCS_BUCKET!;
+    const bucket = storage.bucket(bucketName);
 
-    const d = await s3.upload(uploadParams).promise();
+    // Sending the upload request
+    const d = await bucket.upload(`./image_to_upload.jpeg`, {
+      destination: `images/image_to_upload.jpeg`,
+    });
 
-    return { ...data, url: d.Location };
+    // console.log(d[0].name);
+
+    return { ...data, url: d[0].baseUrl };
   } catch (e) {
     return data;
   }
